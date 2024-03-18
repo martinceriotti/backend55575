36195866f5c8298960350a8e754293be31d1f269
Martin Ceriotti <martinceriotti@gmail.com>
2024-03-18 10:27:10 -0300

    login failed seguridad

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { createUser, getUsersByEmailPassword} from '../controllers/users.controller.js'
-const router =  toAsyncRouter(Router());
-
-router.post("/register", createUser)
-router.post("/login", getUsersByEmailPassword)
 import toAsyncRouter from 'async-express-decorator';
 
+const router =  toAsyncRouter(Router());
+router.post("/register", createUser)
+router.post("/login", getUsersByEmailPassword)
 export default router;
